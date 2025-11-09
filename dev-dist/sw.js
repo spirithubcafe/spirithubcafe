cbba03b9c410bf3405e29607351f438787a1578f
alsalmisaid <alsalmi.said@gmail.com>
2025-11-09 11:55:41 +0400

    feat: enhance ProfessionalHeroSlider with subtitle support and responsive adjustments; update CategoriesSection layout for improved spacing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.h8hur0rcpjo"
+    "revision": "0.f4t8aqfveqo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
