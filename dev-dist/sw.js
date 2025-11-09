e7ad0a7ebe556c939e4813326d8de39f8853ffa4
alsalmisaid <alsalmi.said@gmail.com>
2025-11-09 10:43:53 +0400

    refactor: simplify CheckoutPage layout by removing unnecessary card components and adjusting styles

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.df3iifrntt"
+    "revision": "0.btlcskkk6a8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
