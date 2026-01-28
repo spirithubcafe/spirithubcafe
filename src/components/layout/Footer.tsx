6b9d3cc23751152d69b894e326f38bf2ae61b018
alsalmisaid <alsalmi.said@gmail.com>
2026-01-28 16:09:07 +0400

    refactor: remove unused useEffect import from Footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Facebook, Instagram, Mail, MessageCircle } from 'lucide-react';
 import { useApp } from '../../hooks/useApp';
